80128081b1f91dc7861912ab98dfa30bbae301a2
Naman Sharma <namansharma3007c@gmail.com>
2024-02-24 10:02:44 +0530

    icon changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SATURN from '../assets/saturn.png';
 
 // Navbar component
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
     <div className="flex justify-center py-3 w-full">
       <div className="flex gap-1 items-center">
         <img
-          src="/saturn.png"
+          src={SATURN}
           alt="img"
           className="w-10 h-10 filter brightness-0 invert grayscale"
         />
